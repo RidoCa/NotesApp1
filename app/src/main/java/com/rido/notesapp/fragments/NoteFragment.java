834e277f192e2beb8fd0664f885b7f58507f2687
Rido <ridoca22@gmail.com>
2019-03-31 12:11:51 +0700

    Initial commit

---
+++ app/src/main/java/com/rido/notesapp/fragments/NoteFragment.java
@@ -0,0 +1,109 @@
+package com.rido.notesapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rido.notesapp.Adapters.NoteAdapter;
+import com.rido.notesapp.R;
+import com.rido.notesapp.models.Constant;
+import com.rido.notesapp.models.Data;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NoteFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private NoteAdapter adapter;
+    private OnNoteFragmentListener listener;
+
+    public OnNoteFragmentListener getListener() {
+        return listener;
+    }
+
+    public void setListener(OnNoteFragmentListener listener) {
+        this.listener = listener;
+    }
+
+    public NoteFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+        recyclerView = view.findViewById(R.id.rv_notes);
+
+        adapter = new NoteAdapter(getContext(), Data.getNotes());
+        recyclerView.setAdapter(adapter);
+        displayAsList();
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_note, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void displayAsList() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        adapter.setLayout(Constant.LAYOUT_MODE_LIST);
+    }
+
+    private void displayAsGrid() {
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter.setLayout(Constant.LAYOUT_MODE_GRID);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_show_list:
+                displayAsList();
+                return true;
+            case R.id.action_show_grid:
+                displayAsGrid();
+                return true;
+
+            case R.id.action_logout:
+                Log.i(TAG, "Logout clicked");
+                if(listener != null){
+                    listener.onLogoutClick();
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public interface OnNoteFragmentListener {
+        void onLogoutClick();
+    }
+}
---
+++ app/src/main/java/com/rido/notesapp/models/Session.java
@@ -0,0 +1,43 @@
+package com.rido.notesapp.models;
+
+public class Session {
+
+    private Setting settings;
+    private String user;
+
+    public Session(Setting settings) {
+        this.settings = settings;
+        user = settings.getUser();
+    }
+
+    public User doLogin(String username, String password) {
+        User foundUser = null;
+        for (User user : Data.getUsers()) {
+            if (username.equals(user.getUsername())
+                    && password.equals(user.getPassword())) {
+                foundUser = user;
+                break;
+            }
+        }
+        return foundUser;
+    }
+
+    public void doLogout() {
+        settings.removeUser();
+        this.user = null;
+    }
+
+    public boolean isLogin() {
+        return user != null;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        settings.setUser(user);
+        this.user = user;
+    }
+
+}
